2dac969d05f6ce49504fd35b446d99bc99cf02fa
Shea Parkes <shea.parkes@milliman.com>
2024-12-09 23:20:05 -0500

    Parse the entry/exit states into arrays.
    Do this because we need this more deeply nested structure to represent
    the real plausible truths here.

--- load_moves.js
+++ load_moves.js
@@ -2,16 +2,21 @@ import Papa from "https://cdn.skypack.dev/papaparse@5.4.1";
 import * as R from "https://cdn.skypack.dev/rambda";
 window.R = R;
 
-const cleaner = R.filter(R.pipe(R.propEq("", "Entry States"), R.not));
+const scrub_moves = R.pipe(
+  R.filter(R.prop("Entry States")),
+  R.map(
+    R.evolve({ "Entry States": R.split(","), "Exit States": R.split(",") }),
+  ),
+);
 
 Papa.parse("moves.csv", {
   download: true,
   header: true,
   complete: function (results) {
-    console.log("Parsed Results:", results.data);
-    window.moves = cleaner(results.data);
+    console.log("Raw results of parsing moves.csv", results.data);
+    window.MOVES = scrub_moves(results.data);
   },
   error: function (error) {
-    console.error("Error:", error);
+    console.error("Error parsing moves.csv:", error);
   },
 });
