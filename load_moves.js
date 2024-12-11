74f205a1cf6b2493ee0b5f594b5e2759a5e05233
Shea Parkes <shea.parkes@milliman.com>
2024-12-10 20:27:28 -0500

    Correct embedded array parsing.
    Do this because I wasn't trimming off the whitespace around the commas.

--- load_moves.js
+++ load_moves.js
@@ -2,11 +2,10 @@ import Papa from "papaparse";
 import * as R from "rambda";
 window.R = R;
 
-const scrub_moves = R.pipe(
+const splitStates = R.pipe(R.split(","), R.map(R.trim));
+const scrubMoves = R.pipe(
   R.filter(R.prop("Entry States")),
-  R.map(
-    R.evolve({ "Entry States": R.split(","), "Exit States": R.split(",") }),
-  ),
+  R.map(R.evolve({ "Entry States": splitStates, "Exit States": splitStates })),
 );
 
 Papa.parse("moves.csv", {
@@ -14,7 +13,7 @@ Papa.parse("moves.csv", {
   header: true,
   complete: function (results) {
     console.log("Raw results of parsing moves.csv", results.data);
-    window.MOVES = scrub_moves(results.data);
+    window.MOVES = scrubMoves(results.data);
   },
   error: function (error) {
     console.error("Error parsing moves.csv:", error);
