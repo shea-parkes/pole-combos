247215ccd2313cc8c48809649b1440e7d4d593d6
Shea Parkes <shea.parkes@milliman.com>
2024-12-10 19:56:46 -0500

    Constrain overall altitude.
    Do this because the floor and ceiling do exist.

--- generate_combo.js
+++ generate_combo.js
@@ -1,6 +1,6 @@
 import * as R from "rambda";
 
-const altitudeChange = {
+const altitudeChanges = {
   Up: 1,
   Stationary: 0,
   Down: -1,
@@ -21,9 +21,15 @@ const sampleOne = (arr) => arr[Math.floor(Math.random() * arr.length)];
 const getTrickName = R.path("Pole Trick");
 const getTransitions = (src, dst) =>
   R.intersection(src["Exit States"], dst["Entry States"]);
+const getAltitudeChange = R.pipe(
+  R.path("Altitude Change"),
+  R.flip(R.path)(altitudeChanges),
+);
+const getRange = R.converge(R.subtract, [Math.max, Math.min]);
 
 function generateCombo(size, arr) {
   const combo = [sampleOne(arr)];
+  const heights = [0];
   while (combo.length < size) {
     const existingNames = R.map(getTrickName, combo);
     const checkExisting = R.pipe(
@@ -37,12 +43,17 @@ function generateCombo(size, arr) {
       if (R.not(getTransitions(R.last(combo), move))) {
         continue;
       }
+      const newAltitude = R.add(R.last(heights), getAltitudeChange(move));
+      if (getRange(R.append(newAltitude, heights)) > MAX_HEIGHT_RANGE) {
+        continue;
+      }
       combo.push(move);
+      heights.push(newAltitude);
       break;
     }
   }
 
-  return combo;
+  return [combo, heights];
 }
 
 window.generateCombo = generateCombo;
