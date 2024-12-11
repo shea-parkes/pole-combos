ce438d507523f20be2b2e79286410110f164ecb5
Shea Parkes <shea.parkes@milliman.com>
2024-12-10 19:23:23 -0500

    Start combo generator logic.
    Do this to iterate towards the key business logic.

--- generate_combo.js
+++ generate_combo.js
@@ -1,5 +1,13 @@
 import * as R from "rambda";
 
+const altitudeChange = {
+  Up: 1,
+  Stationary: 0,
+  Down: -1,
+};
+
+const MAX_HEIGHT_RANGE = 3;
+
 const shuffleArray = (arr) => {
   const newArr = arr.slice();
   for (let i = newArr.length - 1; i >= 0; i--) {
@@ -9,4 +17,29 @@ const shuffleArray = (arr) => {
   return newArr;
 };
 
-window.shuffleArray = shuffleArray;
+const sampleOne = (arr) => arr[Math.floor(Math.random() * arr.length)];
+const getTrickName = R.path("Pole Trick");
+
+function generateCombo(size, arr) {
+  const combo = [sampleOne(arr)];
+  while (combo.length < size) {
+    for (const move of shuffleArray(arr)) {
+      if (R.includes(getTrickName(move), R.map(getTrickName, combo))) {
+        continue;
+      }
+      if (
+        R.not(
+          R.intersection(move["Entry States"], R.last(combo)["Exit States"]),
+        )
+      ) {
+        continue;
+      }
+      combo.push(move);
+      break;
+    }
+  }
+
+  return combo;
+}
+
+window.generateCombo = generateCombo;
