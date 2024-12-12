7927339b5169bef788346eb6abf8f97b009c6ca8
Shea Parkes <shea.parkes@milliman.com>
2024-12-11 20:18:19 -0500

    Create required move selector.
    Do this so we can populate it and then try it out.

--- load_moves.js
+++ load_moves.js
@@ -7,6 +7,15 @@ const scrubMoves = R.pipe(
   R.filter(R.prop("Entry States")),
   R.map(R.evolve({ "Entry States": splitStates, "Exit States": splitStates })),
 );
+const requiredMoveSelect = document.getElementById("requiredMoveSelect");
+const addMoveOptions = (arr) => {
+  for (const move of arr) {
+    const option = document.createElement("option");
+    option.value = move;
+    option.textContent = move;
+    requiredMoveSelect.appendChild(option);
+  }
+};
 
 Papa.parse("moves.csv", {
   download: true,
@@ -14,6 +23,11 @@ Papa.parse("moves.csv", {
   complete: function (results) {
     console.log("Raw results of parsing moves.csv", results.data);
     window.MOVES = scrubMoves(results.data);
+    R.pipe(
+      R.map(R.prop("Pole Trick")),
+      R.sort(R.comparator(R.lt)),
+      addMoveOptions,
+    )(window.MOVES);
   },
   error: function (error) {
     console.error("Error parsing moves.csv:", error);
